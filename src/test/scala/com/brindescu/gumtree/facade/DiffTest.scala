3230f5367dc3dedb3321df7c18184db6368d2470
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-10-28 17:53:05 -0700

    Added test for the diff

---
+++ src/test/scala/com/brindescu/gumtree/facade/DiffTest.scala
@@ -0,0 +1,33 @@
+package com.brindescu.gumtree.facade
+
+import com.brindescu.gumtree.facade.ASTDiff._
+import com.github.gumtreediff.tree.ITree
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.JavaConversions._
+
+class DiffTest extends FlatSpec with Matchers {
+
+  private val diff = getDiff("public class A{}", "public class A{}")
+
+  it should "have the correct matchings, left to right, for the null diff" in {
+    processTree(diff.getLeftTree)
+  }
+
+  it should "have the correct matchings, right to left, for the null diff" in {
+    processTree(diff.getRightTree)
+  }
+
+  private def processTree(n: ITree): Any = {
+    val matchedNode = diff.getMatch(n)
+    assertNodeEquality(n, matchedNode)
+    asScalaBuffer(n.getChildren).foreach(n => processTree(n))
+  }
+
+  private def assertNodeEquality(a: ITree, b: ITree): Unit = {
+    getASTNode(b).getClass should equal(getASTNode(a).getClass)
+    getASTNode(b).getNodeType should equal(getASTNode(a).getNodeType)
+    getASTNode(b).getStartPosition should equal(getASTNode(a).getStartPosition)
+    getASTNode(b).getLength should equal(getASTNode(a).getLength)
+  }
+}
