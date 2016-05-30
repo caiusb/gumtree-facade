d4a6f63c04f18649b34d71b3481f31ff5b51d9ed
Caius Brindescu <brindesc@oregonstate.edu>
2016-05-30 13:14:44 -0700

    Wrapping the Actions as well

--- src/main/scala/com/brindescu/gumtree/facade/Gumtree.scala
+++ src/main/scala/com/brindescu/gumtree/facade/Gumtree.scala
@@ -1,13 +1,22 @@
 package com.brindescu.gumtree.facade
 
+import com.github.gumtreediff.actions.model.Action
 import com.github.gumtreediff.tree.ITree
 
 object Gumtree {
 
 	implicit def wrapTree(t: ITree) = new RichTree(t)
+	implicit def wrapAction(a: Action) = new RichAction(a)
 }
+
+import Gumtree._
 
 class RichTree(tree: ITree) {
 
 	def getNode = ASTDiff.getASTNode(tree)
+}
+
+class RichAction(action: Action) {
+
+	def getASTNode = action.getNode.getNode
 }
