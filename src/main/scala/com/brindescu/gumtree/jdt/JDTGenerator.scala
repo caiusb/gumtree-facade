539a5da4c693ee18b374136684c8ca1ed882bfb9
Caius Brindescu <brindesc@oregonstate.edu>
2016-09-09 17:11:29 -0700

    Custom generator
    I don’t remember why I wrote it, but I did. So here it is.

--- src/main/scala/com/brindescu/gumtree/jdt/JDTGenerator.scala
+++ src/main/scala/com/brindescu/gumtree/jdt/JDTGenerator.scala
@@ -1,7 +1,27 @@
 package com.brindescu.gumtree.jdt
 
-import com.github.gumtreediff.gen.jdt.{AbstractJdtTreeGenerator, AbstractJdtVisitor}
+import java.io.Reader
+import java.util
 
-object JDTGenerator extends AbstractJdtTreeGenerator {
-  override def createVisitor(): AbstractJdtVisitor = new Visitor
+import com.github.gumtreediff.gen.TreeGenerator
+import org.eclipse.jdt.core.JavaCore
+import org.eclipse.jdt.core.dom.{AST, ASTParser}
+
+object JDTGenerator extends TreeGenerator {
+
+  override def generate(r: Reader) = {
+    val parser = ASTParser.newParser(AST.JLS8)
+    parser.setKind(ASTParser.K_COMPILATION_UNIT)
+    parser.setBindingsRecovery(true)
+    parser.setResolveBindings(true)
+    val pOptions = JavaCore.getOptions.asInstanceOf[util.Hashtable[String, String]]
+    pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8)
+    pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_8)
+    pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8)
+    parser.setCompilerOptions(pOptions)
+    parser.setSource(Stream.continually(r.read).takeWhile(_ != -1).map(_.toChar).toArray)
+    val v = new Visitor
+    parser.createAST(null).accept(v)
+    v.getTreeContext
+  }
 }
