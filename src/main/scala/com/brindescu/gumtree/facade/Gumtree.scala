44cd0d133bda4054257e63f7ba90119788b19862
Caius Brindescu <brindesc@oregonstate.edu>
2016-09-11 16:46:25 -0700

    Implicit wrappings

--- src/main/scala/com/brindescu/gumtree/facade/Gumtree.scala
+++ src/main/scala/com/brindescu/gumtree/facade/Gumtree.scala
@@ -1,12 +1,18 @@
 package com.brindescu.gumtree.facade
 
+import com.brindescu.gumtree.c.CTree
+import com.brindescu.gumtree.jdt.JavaTree
 import com.github.gumtreediff.actions.model.Action
 import com.github.gumtreediff.tree.ITree
+import org.eclipse.cdt.core.dom.ast.IASTNode
+import org.eclipse.jdt.core.dom.ASTNode
 
 object Gumtree {
 
 	implicit def wrapTree(t: ITree) = new RichTree(t)
 	implicit def wrapAction(a: Action) = new RichAction(a)
+	implicit def wrapJavaAST(a: ASTNode) = new JavaTree(a)
+	implicit def wrapCAST(a: IASTNode) = new CTree(a)
 }
 
 import com.brindescu.gumtree.facade.Gumtree._
