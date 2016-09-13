27553e08e4ee2d82a8068ba1563c6596ffc4d97a
Caius Brindescu <brindesc@oregonstate.edu>
2016-09-12 22:52:55 -0700

    Implemented C AST interface

--- src/main/scala/com/brindescu/gumtree/c/CTree.scala
+++ src/main/scala/com/brindescu/gumtree/c/CTree.scala
@@ -1,27 +1,32 @@
 package com.brindescu.gumtree.c
 
-import com.brindescu.gumtree.facade.SuperTree
-import org.eclipse.cdt.core.dom.ast.IASTNode
+import com.brindescu.gumtree.facade.{SuperBlock, SuperMethod, SuperStatement, SuperTree}
+import org.eclipse.cdt.core.dom.ast.{IASTCompoundStatement, IASTFunctionDefinition, IASTNode, IASTStatement}
 
 class CTree(n: IASTNode) extends SuperTree {
 
 	override def getLineNumber(): Int =
 		n.getFileLocation.getStartingLineNumber
 
-	override def getSourceRange(): List[Int] = ???
+	override def getSourceRange(): List[Int] =
+		Range(n.getFileLocation.getStartingLineNumber, n.getFileLocation.getEndingLineNumber+1).toList
 
 	override def getParent(): SuperTree =
 		CTree(n.getParent)
 
-	override def getChildren(): List[SuperTree] = ???
+	override def getChildren(): List[SuperTree] = n.getChildren.map { CTree(_) }.toList
 
 	override def getUnderlyingNode(): IASTNode = n
 
-	override def getEnclosingClass(): Option[SuperTree] = ???
+	override def getEnclosingClass(): Option[SuperTree] = None
 
-	override def getNodeType(): String = ???
+	override def getNodeType(): String = n.getClass.getSimpleName
 
-	override def getEnclosingMethod(): Option[SuperTree] = ???
+	override def getEnclosingMethod(): Option[SuperTree] = n match {
+		case f: IASTFunctionDefinition => Some(CTree(f))
+		case n: IASTNode if getParent() != null => Some(CTree(n))
+		case _ => None
+	}
 
 	override def equals(other: Any): Boolean =
 		other match {
@@ -34,6 +39,21 @@ class CTree(n: IASTNode) extends SuperTree {
 }
 
 object CTree {
-	def apply(n: IASTNode) = new CTree(n)
+	def apply(n: IASTNode) = n match {
+		case b: IASTCompoundStatement => new CBlock(b)
+		case s: IASTStatement => new CStatement(s)
+		case m: IASTFunctionDefinition => new CMethod(m)
+		case n => new CTree(n)
+	}
 	def unapply(t: CTree): Option[IASTNode] = Some(t.getUnderlyingNode)
+}
+
+class CStatement(s: IASTStatement) extends CTree(s) with SuperStatement
+
+class CMethod(f: IASTFunctionDefinition) extends CTree(f) with SuperMethod {
+	override def getIdentifier(): String = f.getDeclarator.getName.toString
+}
+
+class CBlock(b: IASTCompoundStatement) extends CTree(b) with SuperBlock {
+	override def getBlockInfo(): String = ""
 }
